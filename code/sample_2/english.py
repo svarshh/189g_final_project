c5ca0f81e72063874681c8e5e570c382cb332e29
Varsha <svarshh@users.noreply.github.com>
2025-05-20 11:26:16 -0700

    yes/no questions and fact questions prototype

---
+++ code/sample_2/english.py
@@ -0,0 +1,122 @@
+from pyswip import Prolog
+from queue import deque
+
+class noun:
+    def __init__(self, name: str):
+        self.name = name
+    def __str__(self):
+        return f"{self.name}"
+
+class verb:
+    def __init__(self, name: str, p_simple: str):
+        self.name = name
+        self.ps = p_simple
+    def present_simple(self):
+        return self.ps
+    def __str__(self):
+        return f"{self.name}"
+
+class propernoun(noun):
+    def __init__(self, name: str):
+        super().__init__(name)  # Capitalize here
+    def __str__(self):
+        return f"{self.name.capitalize()}"
+
+class person(propernoun):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+class time(noun):
+    def __init__(self, name: str):
+        super().__init__(name)
+    # def __str__(self):
+    #     return f"{self.name}"
+
+class place(noun):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+class proper_place(propernoun):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+class things(noun):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+
+# can be used to decide which auxillaries to use.
+def select_auxiliary(subject_person, subject_number, tense):
+    if tense == "present_simple":
+        if subject_person == 3 and subject_number == "singular":
+            return "does"
+        else:
+            return "do"
+    elif tense == "past_simple":
+        return "did"
+    elif tense == "present_continuous":
+        if subject_person == 1 and subject_number == "singular":
+            return "am"
+        elif subject_person == 3 and subject_number == "singular":
+            return "is"
+        else:
+            return "are"
+    elif tense == "past_continuous":
+        if subject_person in [1,3] and subject_number == "singular":
+            return "was"
+        else:
+            return "were"
+    elif tense == "future_simple":
+        return "will"
+    
+
+gc = {
+    "WH": ["who", "what"],
+    "AUX": [verb("do","does"), verb("is", "are")],
+    "SUBJ": [person("alice"), person("bob"), person("claire") ],
+    "VERB": [verb("like", "likes")],
+    "OBJ": [things("pizza"), things("sushi")],
+    "NOUN_RELATION": []
+}
+
+# Questioning Facts
+# wh + aux + subj + verb
+prolog = Prolog()
+prolog.consult("logic.pl")
+
+for wh in gc["WH"]:
+    for aux in gc["AUX"]:
+        for subj in gc["SUBJ"]:
+            for v in gc["VERB"]:
+                obj = (deque(prolog.query(f"{wh}(X, {subj.name})")))
+                if not obj:
+                    break
+                print(f"Q: {wh.capitalize()} {aux} {subj} {v}?")
+                print(f"A: {subj} {v.present_simple()} {obj[0]['X']}.")
+                print(" ")
+
+
+# Yes/No Questions
+# AUX + SUBJ + VERB + OBJ
+for aux in gc["AUX"]:
+    for subj in gc["SUBJ"]:
+        for v in gc["VERB"]:
+            for obj in gc["OBJ"]:
+                ans = "No."
+                if(deque(prolog.query(f"verb_relation({v}, {subj.name}, {obj.name})"))):
+                    ans = "Yes."
+                print(f"Q: {aux.name.capitalize()} {subj} {v} {obj}?")
+                print(f"A: {ans}")
+                print(" ")
+                   
+                
+
+
+
+
+
+
+
+
+
+
--- code/sample_2/logic.pl
+++ code/sample_2/logic.pl
@@ -1,12 +1,37 @@
-type(root).
+% inheritance structure
+
 inherit(noun, root).
 inherit(verb, root).
 inherit(pronoun, root).
-inherit(number, noun).
-inherit(person, noun).
-inherit(class, noun).
-inherit(grade, number).
-inherit(test, noun).
-inherit(prerequisites, noun).
-inherit(score, verb).
+inherit(proper_noun, noun).
+inherit(person, proper_noun).
+inherit(food, things.)
 
+
+% facts and rules
+
+% Facts, unary type relations
+
+type(person, alice).
+type(person, bob).
+type(person, claire).
+
+type(food, pizza).
+type(food, sushi).
+
+type(verb, like).
+
+% binary verb relations (subj obj pairs)
+
+verb_relation(like, alice, pizza).
+verb_relation(like, bob, pizza).
+verb_relation(like, bob, sushi).
+verb_relation(like, claire, sushi).
+verb_relation(like, claire, bob).
+
+% Rule
+possessive_relation(friend, A, B) :- verb_relation(like, A, F), verb_relation(like, B, F), type(food, F), A \= B.
+
+% builtin natural language rules
+who(X, Y) :- type(person, X), verb_relation(_, Y, X).
+what(X, Y) :- type(food, X), verb_relation(_, Y, X).
