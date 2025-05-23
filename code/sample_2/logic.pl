55172fceb6d62fb5e1b88b44ae2e3af56788de32
Varsha <svarshh@users.noreply.github.com>
2025-05-22 20:50:52 -0700

    prototype only using prolog, added limited tense and singular/plural support

--- code/sample_2/logic.pl
+++ code/sample_2/logic.pl
@@ -1,38 +1,253 @@
-% inheritance structure
 
-% inherit(noun, root).
-% inherit(verb, root).
-% inherit(pronoun, root).
-% inherit(proper_noun, noun).
-% inherit(person, proper_noun).
-% inhreit(things, noun).
-% inherit(food, things).
+% ------------Builtins--------------
+% lex
+lex(wh, 'who').
+lex(wh, 'what').
+lex(wh, 'where').
+lex(wh, 'when').
+lex(wh, 'which').
+lex(wh, 'how').
+
+lex(modal, 'can').
+lex(modal, 'could').
+lex(modal, 'will').
+lex(modal, 'would').
+lex(modal, 'shall').
+lex(modal, 'should').
+lex(modal, 'may').
+lex(modal, 'might').
+lex(modal, 'must').
+lex(modal, 'ought_to').
+
+lex(pronoun, 'i').
+lex(pronoun, 'you').
+lex(pronoun, 'he').
+lex(pronoun, 'she').
+lex(pronoun, 'it').
+lex(pronoun, 'we').
+lex(pronoun, 'they').
+
+lex(subject, 'bob').
+lex(subject, 'alice').
+
+lex(object, 'pizza').
+lex(object, 'food').
+lex(object, 'sushi').
 
 
-% facts and rules
+% -------------- Inheritance Structure ---------------
 
-% Facts, unary type relations
+inherits(common_noun, noun).
+inherits(concrete_noun, common_noun).
+inherits(countable_noun, concrete_noun).
+inherits(uncountable_noun, concrete_noun).
+inherits(abstract_noun, common_noun).
+inherits(countable_abstract, abstract_noun).
+inherits(uncountable_abstract, abstract_noun).
 
-type(person, alice).
-type(person, bob).
-type(person, claire).
+inherits(proper_noun, noun).
+inherits(person_name, proper_noun).
+inherits(place_name, proper_noun).
+inherits(organization_name, proper_noun).
+inherits(specific_title_event, proper_noun).
 
-type(food, pizza).
-type(food, sushi).
-type(verb, like).
+inherits(collective_noun, noun).
+inherits(compound_noun, noun).
+inherits(gerund, noun).
+inherits(possessive_noun, noun).
 
-% binary verb relations (subj obj pairs)
+inherits(number, common_noun).
 
-verb_relation(like, alice, pizza).
-verb_relation(like, bob, pizza).
-verb_relation(like, bob, sushi).
-verb_relation(like, claire, sushi).
-verb_relation(like, claire, bob).
+% ----------------------- auxiliary section ----------------------------
 
-% Rule
-% B is friend of A
-possessive_relation(friend, A, B) :- verb_relation(like, A, F), verb_relation(like, B, F), type(food, F), A \= B.
+% choose_auxiliary(+Tense, +Person, +Number, -Aux)
 
-% builtin natural language rules
-who(X, Y) :- type(person, X), verb_relation(_, Y, X).
-what(X, Y) :- type(food, X), verb_relation(_, Y, X).
+% Simple aspect (do-support)
+auxiliary(present_simple, third, singular, 'does').
+auxiliary(present_simple, _, plural, 'do').
+auxiliary(present_simple, first, singular, 'do').
+auxiliary(present_simple, second, singular, 'do').
+auxiliary(past_simple, _, _, 'did').
+
+% Progressive aspect (be)
+auxiliary(present_progressive, first, singular, 'am').
+auxiliary(present_progressive, third, singular, 'is').
+auxiliary(present_progressive, _, plural, 'are').
+auxiliary(past_progressive, third, singular, 'was').
+auxiliary(past_progressive, first, singular,  'was').
+auxiliary(past_progressive, _, plural,  'were').
+auxiliary(past_progressive, second, singular, 'were').
+
+% Perfect aspect (have)
+auxiliary(present_perfect, third, singular, 'has').
+auxiliary(present_perfect, _, _, 'have').
+auxiliary(past_perfect, _, _, 'had').
+
+% Choose the tense of the question verb based on the auxillary used.
+
+% Simple aspect
+aux_tense_map(do, base).
+aux_tense_map(does, base).
+aux_tense_map(did, base).
+
+% Progressive aspect
+aux_tense_map(am, present_progressive).
+aux_tense_map(is, present_progressive).
+aux_tense_map(are, present_progressive).
+aux_tense_map(was, past_progressive).
+aux_tense_map(were, past_progressive).
+
+% Perfect aspect
+aux_tense_map(has, past_participle).
+aux_tense_map(have, past_participle).
+aux_tense_map(had, past_participle).
+
+
+
+% check whether an object is inherited from a super class.  example: check whether bob is a noun.
+
+
+is_type_of(Instance, SuperType) :-
+    type(Type, Instance),
+    inherits_or_same(Type, SuperType).
+
+inherits_or_same(Type, Type).
+inherits_or_same(SubType, SuperType) :-
+    inherits(SubType, Parent),
+    inherits_or_same(Parent, SuperType).
+
+
+
+% ----------------------------------------------USER-DEFINED---------------------------------------------------------- 
+
+% INHERITANCE
+
+inherits(person, proper_noun).
+inherits(place, proper_noun).
+inherits(grade, number).
+inherits(class, countable_noun).
+inherits(prerequisite, abstract_noun).
+
+
+
+% TYPE ASSIGNMENTS
+
+type(person, 'Bob').
+type(grade, 'A+').
+type(grade, 'A').
+type(grade, 'B').
+
+type(class, '36a').
+type(class, '36b').
+type(class, '36c').
+type(class, '20').
+type(class, '32a').
+
+type(verb, 'take').
+type(verb, 'like').
+
+type(person, 'Bob').
+type(person, 'Alice').
+
+type(place, 'San Francisco').
+
+type(countable_noun, 'medicine').
+
+% PERSON POV
+
+pov(first, 'user').
+pov(third, 'Bob').
+pov(third, 'Alice').
+
+% TELLS WHETHER THERE IS A FINITE CHOICE OR NOT
+
+choice(person).
+choice(class).
+choice(place).
+
+% SINGULAR/PLURAL SPECIFICATION FOR NOUNS
+
+collective(singular, 'Bob').
+collective(singular, 'Alice').
+collective(singular, 'San Francisco').
+collective(plural, prerequisite).
+
+% TENSE DEFINITION FOR ANY VERB
+
+tense('take', _{
+    base: take,
+    present_simple: takes,
+    past_simple: took,
+    present_progressive: taking,
+    past_progressive: was_taking 
+}).
+
+tense('like', _{
+    base: like,
+    present_simple: likes,
+    past_simple: liked,
+    present_progressive: liking,
+    past_progressive: was_liking  
+}).
+
+tense('go', _{
+    base: go,
+    present_simple: goes,
+    past_simple: 'went to',
+    present_progressive: going,
+    past_progressive: was_going  
+}).
+
+
+
+% VERB RELATIONS
+
+verb_relation(present_simple, 'like', '36a', 'Bob').
+verb_relation(past_simple, 'take', 'medicine', 'Bob').
+verb_relation(past_simple, 'like', 'Alice', 'Bob').
+verb_relation(past_simple, 'go', 'San Francisco', 'Bob').
+
+
+% CONDITIONAL VERB RELATIONS
+
+conditional_verb_relation('take', X, Y) :- type(class, Y), type(person, X), 
+                                        property(Z, prerequisite, Y), 
+                                        past_verb_relation('took', X, Z).
+
+% scheduling binary relations
+
+property("20", prerequisites, [[]]).
+property("36a", prerequisites, [["32a"],["placement"]]).
+property("36b", prerequisites, [["36a"]]).
+property("36c", prerequisites, [["36b", "20"]]).
+
+
+lex(subject, X) :- verb_relation(_, _, _, X).
+
+match(X, Y, Z) :- is_type_of(X, 'person'), Y = 'who', Z = Y;
+                is_type_of(X, 'noun'), Y = 'which', type(Z1, X), choice(Z1), atomic_list_concat([Y, " ", Z1], Z);
+                is_type_of(X, 'noun'), type(Z1, X),  \+ choice(Z1), Y = 'what', Z=Y;
+                is_type_of(X, 'place'), Y = 'where', Z=Y.
+             
+
+% ------------------------BUILTI-IN------------------------------
+% QUESTION ANSWER CONSTRAINTS.  BUILT-IN
+% wh + (OBJ) + aux + subj + verb 
+
+question(Q, A) :-  
+                lex(wh, Wh), 
+                verb_relation(Tense, Verb, Obj, Subj), 
+                match(Obj, Wh, W), 
+                pov(Pov, Subj), 
+                collective(Collective, Subj), 
+                auxiliary(Tense, Pov, Collective, Aux), 
+                aux_tense_map(Aux, Question_tense), 
+                tense(Verb, V), 
+                get_dict(Question_tense, V, Verb_final),
+
+                atomic_list_concat([W, " ", Aux, " " , Subj, " ", Verb_final, "? "], Q),
+
+                get_dict(Tense, V, Ans_verb), 
+
+                atomic_list_concat([Subj, " ", Ans_verb, " ", Obj,"."], A).
+
